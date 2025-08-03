572f77108d6c42e92c9904b273a21825d93be417
Guilherme Saliba <salibagui19@gmail.com>
2025-08-03 19:29:02 -0300

    test: testing pre-commit hook

---
+++ src/app.ts
@@ -0,0 +1,10 @@
+import express from 'express';
+
+const app = express();
+const port = process.env.PORT || 8000;
+
+app.get('/', (req, res) => {
+  res.send('index');
+});
+
+app.listen(port, () => console.log(`App running at http://localhost:${port}`));
