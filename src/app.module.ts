4ad200d4cbd89444fb2d2daaaeaf4de82da9e587
Guilherme Saliba <salibagui19@gmail.com>
2025-08-20 19:43:39 -0300

    chore: update app module

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,24 +1,25 @@
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
-
 import { ConfigModule } from '@nestjs/config';
-
 import { UsersModule } from './users/users.module';
 import { AppointmentsModule } from './appointments/appointments.module';
 import { AuthModule } from './auth/auth.module';
 import { PaymentsModule } from './payments/payments.module';
+import { SharedModule } from './shared/shared.module';
+import { StudiosModule } from './studios/studios.module';
 
 @Module({
   imports: [
     ConfigModule.forRoot({
       isGlobal: true,
-      envFilePath: ['.env'],
     }),
     UsersModule,
     AppointmentsModule,
     AuthModule,
     PaymentsModule,
+    SharedModule,
+    StudiosModule,
   ],
   controllers: [AppController],
   providers: [AppService],
