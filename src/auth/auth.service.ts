4d6e194a0a6b79a2e37508c729a976ba7aa04369
Guilherme Saliba <salibagui19@gmail.com>
2025-08-21 10:37:54 -0300

    feat: create auth service

--- src/auth/auth.service.ts
+++ src/auth/auth.service.ts
@@ -1,4 +1,34 @@
-import { Injectable } from '@nestjs/common';
+import { Injectable, UnauthorizedException } from '@nestjs/common';
+import { hash, compare } from 'bcrypt';
+import type { UserForAuth } from 'src/users/types/user.types';
+import { UsersService } from 'src/users/users.service';
 
 @Injectable()
-export class AuthService {}
+export class AuthService {
+  constructor(private readonly usersService: UsersService) {}
+
+  async validateUser(
+    username: string,
+    password: string
+  ): Promise<Omit<UserForAuth, 'password'> | null> {
+    const userWithPassword =
+      await this.usersService.findOneWithPassword(username);
+
+    if (!userWithPassword || !userWithPassword.password) {
+      return null;
+    }
+
+    const isPasswordValid = await compare(password, userWithPassword.password);
+    if (!isPasswordValid) {
+      throw new UnauthorizedException();
+    }
+
+    const { password: _, ...user } = userWithPassword; // Remove password from the user object before returning
+    return user;
+  }
+
+  async hashPassword(password: string): Promise<string> {
+    const saltRounds = 12;
+    return await hash(password, saltRounds);
+  }
+}
