73514459b3cea32954e295787e656924963a988a
Guilherme Saliba <salibagui19@gmail.com>
2025-08-21 12:25:38 -0300

    chore: implement tokenized injection for concrete impls.

--- src/users/users.service.ts
+++ src/users/users.service.ts
@@ -1,11 +1,14 @@
-import { Injectable } from '@nestjs/common';
+import { Inject, Injectable } from '@nestjs/common';
 import { UserRepository } from 'src/shared/interfaces/user-repository.interface';
 import type { DomainUser } from 'src/shared/domain/user';
 import type { CreateUserDTO } from 'src/shared/schemas/user.schema';
+import { USER_REPOSITORY } from 'src/shared/constants/injection-tokens';
 
 @Injectable()
 export class UsersService {
-  constructor(private readonly userRepository: UserRepository) {}
+  constructor(
+    @Inject(USER_REPOSITORY) private readonly userRepository: UserRepository
+  ) {}
 
   async registerUser(input: CreateUserDTO): Promise<DomainUser> {
     return this.userRepository.create(input);
