5f222509f5964f018193dbd0606daca6887eade2
Guilherme Saliba <salibagui19@gmail.com>
2025-08-21 12:42:51 -0300

    feat(schema): validate user role with zod

--- src/shared/schemas/user.schema.ts
+++ src/shared/schemas/user.schema.ts
@@ -1,12 +1,15 @@
+import { UserRole } from '@prisma/client';
 import * as z from 'zod';
+
+const userRoleSchema = z.enum(UserRole);
 
 export const CreateUserSchema = z.object({
   email: z.email(),
-  firstName: z.string().optional(),
-  lastName: z.string().optional(),
+  firstName: z.string(),
+  lastName: z.string(),
   password: z.string().min(8).max(100),
   phone: z.string().optional(),
-  role: z.string().optional(),
+  role: userRoleSchema.optional().default(UserRole.ARTIST),
 });
 
 export type CreateUserDTO = z.infer<typeof CreateUserSchema>;
