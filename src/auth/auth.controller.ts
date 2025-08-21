9e0edbd8383cec30c3e80db6209ac7c58813a7d5
Guilherme Saliba <salibagui19@gmail.com>
2025-08-21 10:36:30 -0300

    feat(guard): create local and jwt auth guards

--- src/auth/auth.controller.ts
+++ src/auth/auth.controller.ts
@@ -1,4 +1,11 @@
-import { Controller } from '@nestjs/common';
+import { Controller, Post, UseGuards } from '@nestjs/common';
+import { LocalAuthGuard } from './guards/local.guard';
 
 @Controller('auth')
-export class AuthController {}
+export class AuthController {
+  // @UseGuards(LocalAuthGuard)
+  // @Post('login')
+  // async login(@Request() req): Promise<any> {
+  //   return this.authService.login(req.user); // req.user comes from strategy
+  // }
+}
