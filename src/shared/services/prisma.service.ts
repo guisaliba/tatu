fb39b9f1a8da1ca392d53169f6c6c42891def690
Guilherme Saliba <salibagui19@gmail.com>
2025-08-21 11:34:58 -0300

    feat: create prisma service

---
+++ src/shared/services/prisma.service.ts
@@ -0,0 +1,45 @@
+import {
+  Injectable,
+  type OnModuleDestroy,
+  type OnModuleInit,
+} from '@nestjs/common';
+import { PrismaClient } from '@prisma/client';
+
+@Injectable()
+export class PrismaService
+  extends PrismaClient
+  implements OnModuleInit, OnModuleDestroy
+{
+  constructor() {
+    super({
+      log:
+        process.env.NODE_ENV === 'development'
+          ? ['query', 'error', 'warn']
+          : ['error'],
+    });
+  }
+
+  async onModuleInit(): Promise<void> {
+    await this.$connect();
+  }
+
+  async onModuleDestroy(): Promise<void> {
+    await this.$disconnect();
+  }
+
+  async clearDatabase(): Promise<void> {
+    if (process.env.NODE_ENV === 'test') {
+      const tablenames = await this.$queryRaw<Array<{ tablename: string }>>`
+        SELECT tablename FROM pg_tables WHERE schemaname='public'
+      `;
+
+      for (const { tablename } of tablenames) {
+        if (tablename !== '_prisma_migrations') {
+          await this.$executeRawUnsafe(
+            `TRUNCATE TABLE "public"."${tablename}" CASCADE;`
+          );
+        }
+      }
+    }
+  }
+}
